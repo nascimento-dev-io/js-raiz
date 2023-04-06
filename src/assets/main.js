8d5977003a5163c4cb91faa5aa3e83c9e718ab55
Jorge Nascimento <nascimento.dev.io@gmail.com>
2023-04-06 10:49:55 -0300

    add render client side with DOM API

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,28 +1,58 @@
 import data from './data.js';
 
+const $cardapios = document.querySelector('.cardapios');
+
 function templateCardapio(menu) {
-  return `
-  <div class="cardapio">
-  <header>
-    <h3> ${menu.title} - ${menu.restaurant.name} </h3>
-  </header>
-  <div class="cardapio-body">
-    <ul>
-      ${menu.sections.map((section) => `<li> ${section.title}</li>`).join('')}
-    </ul>
-  </div>
-</div>
-  `;
+  //   return `
+  //   <div class="cardapio">
+  //   <header>
+  //     <h3> ${menu.title} - ${menu.restaurant.name} </h3>
+  //   </header>
+  //   <div class="cardapio-body">
+  //     <ul>
+  //       ${menu.sections.map((section) => `<li> ${section.title}</li>`).join('')}
+  //     </ul>
+  //   </div>
+  // </div>
+  //   `;
+
+  const $cardapio = document.createElement('div');
+  $cardapio.classList.add('cardapio');
+
+  const $header = document.createElement('header');
+  const $menuTitle = document.createElement('h3');
+  $menuTitle.textContent = `${menu.title} - ${menu.restaurant.name}`;
+  $header.appendChild($menuTitle);
+
+  $cardapio.appendChild($header);
+
+  const $cardapioBody = document.createElement('div');
+  $cardapioBody.classList.add('cardapio-body');
+
+  const $menuList = document.createElement('ul');
+
+  menu.sections.forEach((section) => {
+    const $menuItem = document.createElement('li');
+    $menuItem.textContent = section.title;
+    $menuList.appendChild($menuItem);
+  });
+
+  $cardapioBody.appendChild($menuList);
+  $cardapio.appendChild($cardapioBody);
+
+  return $cardapio;
 }
 
-const templateCardapios = Array.from(data.menus.values())
+const $fragment = document.createDocumentFragment();
+Array.from(data.menus.values())
   .slice(3)
   .map((menu) => ({
     ...menu,
     restaurant: { name: data.restaurants.get(menu.restaurantId).name },
   }))
-  .map(templateCardapio)
-  .join('');
+  .forEach((cardapioElement) =>
+    $fragment.appendChild(templateCardapio(cardapioElement)),
+  );
 
-const $cardapios = document.querySelector('.cardapios');
-$cardapios.insertAdjacentHTML('beforeend', templateCardapios);
+// $cardapios.insertAdjacentHTML('beforeend', templateCardapios);
+$cardapios.appendChild($fragment);
