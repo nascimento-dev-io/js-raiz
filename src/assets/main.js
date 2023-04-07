1c071c02fe22929840532fcf0c6dc39bdf5d5a57
Jorge Nascimento <nascimento.dev.io@gmail.com>
2023-04-07 12:26:10 -0300

    create el function

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,58 +1,69 @@
 import data from './data.js';
 
-const $cardapios = document.querySelector('.cardapios');
-
-function templateCardapio(menu) {
-  //   return `
-  //   <div class="cardapio">
-  //   <header>
-  //     <h3> ${menu.title} - ${menu.restaurant.name} </h3>
-  //   </header>
-  //   <div class="cardapio-body">
-  //     <ul>
-  //       ${menu.sections.map((section) => `<li> ${section.title}</li>`).join('')}
-  //     </ul>
-  //   </div>
-  // </div>
-  //   `;
-
-  const $cardapio = document.createElement('div');
-  $cardapio.classList.add('cardapio');
-
-  const $header = document.createElement('header');
-  const $menuTitle = document.createElement('h3');
-  $menuTitle.textContent = `${menu.title} - ${menu.restaurant.name}`;
-  $header.appendChild($menuTitle);
-
-  $cardapio.appendChild($header);
-
-  const $cardapioBody = document.createElement('div');
-  $cardapioBody.classList.add('cardapio-body');
-
-  const $menuList = document.createElement('ul');
-
-  menu.sections.forEach((section) => {
-    const $menuItem = document.createElement('li');
-    $menuItem.textContent = section.title;
-    $menuList.appendChild($menuItem);
-  });
-
-  $cardapioBody.appendChild($menuList);
-  $cardapio.appendChild($cardapioBody);
-
-  return $cardapio;
+function isChildren(childrens) {
+  return Array.isArray(childrens) || typeof childrens === 'string';
 }
 
+function el(tagName, attrsArr, childrensArr) {
+  /**
+   * Se o segundo argumento (attrs) for um array, significa que não temos atributos
+   * Se o segundo argumento (attrs) for uma string, significa que não temos atributos
+   * Os filhos podem ser ou array ou string
+   
+  */
+
+  const $element = document.createElement(tagName);
+  const childrens = isChildren(attrsArr) ? attrsArr : childrensArr;
+
+  const attrs = !isChildren(attrsArr) ? attrsArr : {};
+
+  Object.entries(attrs).forEach(([key, value]) => {
+    $element.setAttribute(key, value);
+  });
+
+  if (typeof childrens === 'string') {
+    const text = document.createTextNode(childrens);
+    $element.appendChild(text);
+  } else {
+    childrens.forEach(($children) => {
+      $element.appendChild($children);
+    });
+
+    // for (const $children of childrensArr) {
+    //   $element.appendChild($children);
+    // }
+  }
+
+  return $element;
+}
+
+function templateCardapio(menu) {
+  return el(
+    'div',
+    { style: 'background: #FEE0B3; color: #730A0D; padding: 8px; margin:8px' },
+    [
+      el('header', [el('h3', `${menu.title} - ${menu.restaurant.name}`)]),
+      el('div', [
+        el(
+          'ul',
+          menu.sections.map((section) => el('li', `${section.title}`)),
+        ),
+      ]),
+    ],
+  );
+}
+
+const $cardapios = document.querySelector('.cardapios');
+
 const $fragment = document.createDocumentFragment();
+
 Array.from(data.menus.values())
   .slice(3)
   .map((menu) => ({
     ...menu,
     restaurant: { name: data.restaurants.get(menu.restaurantId).name },
   }))
-  .forEach((cardapioElement) =>
-    $fragment.appendChild(templateCardapio(cardapioElement)),
-  );
+  .forEach((menu) => $fragment.appendChild(templateCardapio(menu)));
 
 // $cardapios.insertAdjacentHTML('beforeend', templateCardapios);
 $cardapios.appendChild($fragment);
